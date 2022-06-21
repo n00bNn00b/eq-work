17026ac501091769208c36a5419d10e14b604b63
n00bNn00b <reldewan9@gmail.com>
2022-06-22 01:00:55 +0600

    chart has been implemented through api

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,44 +1,27 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-  CartesianGrid,
-} from "recharts";
+import Graph from "./Graph";
 
 const Chart = () => {
   const [stats, setStats] = useState([]);
+  //   const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://eq-works-task.glitch.me/stats/daily")
-      .then((res) => setStats(res.data));
+      .then((res) => {
+        setStats(res.data);
+        // setData(stats);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-  console.log(stats);
+  //   console.log(stats);
   return (
     <div>
-      {stats.map((stat, index) => (
-        <BarChart key={index} width={600} height={300} data={stat}>
-          <XAxis dataKey={stat.revenue} stroke="#8884d8" />
-          <YAxis />
-          <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} />
-          <Legend
-            width={100}
-            wrapperStyle={{
-              top: 40,
-              right: 20,
-              backgroundColor: "#f5f5f5",
-              border: "1px solid #d5d5d5",
-              borderRadius: 3,
-              lineHeight: "40px",
-            }}
-          />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <Bar dataKey={stat.impressions} fill="#8884d8" barSize={30} />
-        </BarChart>
+      {stats.splice(0, 1).map((stat, index) => (
+        <Graph key={index} stat={stat} stats={stats} />
       ))}
     </div>
   );
---
+++ src/components/Graph.jsx
@@ -0,0 +1,45 @@
+import { info } from "daisyui/src/colors";
+import React from "react";
+import {
+  ComposedChart,
+  Line,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const Graph = ({ stat, stats }) => {
+  const { impressions, clicks, date, revenue } = stats;
+
+  return (
+    <div className="mt-10">
+      <h1 className="text-center font-bold text-2xl">Chart</h1>
+      <div className="mt-20 flex justify-center align-center">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={stats}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="date" scale="band" />
+          <YAxis dataKey="revenue" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="impressions" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="clicks" stroke="#ff7300" />
+        </ComposedChart>
+      </div>
+    </div>
+  );
+};
+
+export default Graph;
